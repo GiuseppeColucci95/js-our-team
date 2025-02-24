c302a4847a2ee2912a280c5569c5a198be2328ae
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-24 15:52:13 +0100

    add dynamic cards with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,87 @@
+//select the dom elements
+const sectionEl = document.getElementById('cards');
+console.log(sectionEl);
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
+
+//cicle for searching for each element of the array 
+for (let i = 0; i < teamMembers.length; i++) {
+  thisMember = teamMembers[i];
+
+  //create HTML markup using a function and save it in a variable
+  const markup = getMarkup(thisMember);
+
+  //add markup to the html
+  sectionEl.innerHTML += markup;
+}
+
+
+
+
+//FUNCTIONS
+
+/**
+ * Function that return a dynamic markup to create a card, from a given object
+ * @param {object} object 
+ * @returns {string} markup
+ */
+function getMarkup(object) {
+
+  const markup = `      
+    <div class="col-4">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-4">
+            <img src="${object.img}" class="img-fluid rounded-start" alt="Card title">
+          </div>
+          <div class="col-8 d-flex align-items-center">
+            <div class="card-body">
+              <h5>${object.name}</h5>
+              <p>${object.role}</p>
+              <p>${object.email}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  return markup;
+}
